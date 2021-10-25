1bbe671e84de2455497b8c03a079a54ac08fc6c3
Kashiii <kashiiitech@gmail.com>
2021-10-25 20:47:13 +0500

    initial commit

---
+++ 02_let_and_const/main.js
@@ -0,0 +1,61 @@
+"use strict"
+
+// var a = 'Test1';
+// let b = 'Test2';
+
+
+/*
+
+function testVar() {
+    var a = 30;
+    if(true) {
+        var a = 50;
+        console.log(a);
+    }
+    console.log(a);
+}
+
+// testVar();  // 50 and 50 
+
+function testLet() {
+    let a = 30; 
+    if(true) {
+        let a = 50;
+        console.log(a);
+    }
+    console.log(a);
+}
+
+// testLet();  // 50 and 30
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+
+// Now use let
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log(i);
+
+*/
+
+
+// const // 
+
+const colors = [];
+
+colors.push('red');
+colors.push('blue');
+
+// colors = 'Green';
+
+console.log(colors);
+
+const userName = "Kashiiitech";
+
+userName = "Kashif";   // Error: const can not be reassign
+
+console.log(userName);

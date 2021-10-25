e87881bd5e1e2f3de8004dfa4b453ef406f5ea83
Kashif Ali <kashiiitech@gmail.com>
2021-10-25 20:54:11 +0500

    initial commit

---
+++ 04_template/main.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// Template and Literals
+
+let name = 'John';
+
+function makeUppercase(word) {
+    return word.toUpperCase();
+}
+
+let template = 
+`<h1>${makeUppercase('Hello')}, ${name}</h1>
+<p>This is a simple template in JavaScript</p>`;
+
+document.getElementById('template').innerHTML = template;

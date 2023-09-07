5347279ea3d4d29ce372ec476fa1382c3a3c5480
kashiiitech <p200648@nu.edu.pk>
2023-09-07 20:18:49 +0500

    circle added

--- CounterApp/index.js
+++ CounterApp/index.js
@@ -12,6 +12,7 @@ function increament() {
   value = parseInt(cnt);
   value++;
   document.querySelector('#counter').innerHTML=value;
+  createDynamicCircle();
 }
 
 
@@ -20,4 +21,31 @@ function decreament() {
   value = parseInt(cnt);
   value--;
   document.querySelector('#counter').innerHTML=value;
+  createDynamicCircle();
 }
+
+
+ // Function to create a dynamic circle with a random color
+ function createDynamicCircle() {
+   const dynamicCircle = document.createElement("div");
+   dynamicCircle.className = "dynamic-circle";
+   dynamicCircle.style.backgroundColor = getRandomColor();
+   dynamicCircle.style.left = `${Math.random() * 90}%`;
+   dynamicCircle.style.top = `${Math.random() * 90}%`;
+   document.body.appendChild(dynamicCircle);
+
+   // Remove the dynamic circle after 1 second
+   setTimeout(() => {
+     dynamicCircle.remove();
+   }, 1000);
+ }
+
+ // Function to generate a random color
+ function getRandomColor() {
+   const letters = "0123456789ABCDEF";
+   let color = "#";
+   for (let i = 0; i < 6; i++) {
+     color += letters[Math.floor(Math.random() * 16)];
+   }
+   return color;
+ }
